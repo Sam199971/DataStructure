690598e4dab62d4ba6252cec0dc1353b840d3fd2
Sam199971 <s88070171@gmail.com>
2018-12-19 21:42:22 +0800

    hw3

--- DataStructure-TwelveWeek-hw3.cpp
+++ DataStructure-TwelveWeek-hw3.cpp
@@ -3,94 +3,132 @@
 #include<time.h>
 
 
-int Heap[3000];
-int Array[3000] = {0};
-int size = 0;
+int Heap[3000],Array[3000], size = 0;
+clock_t start, finish;    
+double t;
 
-void Initial(int array[],int k){
-	for(int i = 0; i < k; i++){
-		array[i] = rand()%100;
-		printf("%d  ",array[i]);
+
+void HUp(int k){
+    
+    if(k == 0){
+		return ;
+	}
+	else if(Heap[k/2] < Heap[k]){
+		return ;
+	}
+	else{
+		int m = Heap[k];
+		Heap[k] = Heap[k/2];
+		Heap[k/2] = m;
+		HUp(k/2);
+	}
+    
+}
+
+
+
+
+void HeapDownSort(int s){
+	int k;
+	if(Heap[2*s] == 0 && Heap[2*s+1] == 0){
+		return ;
+	}else if(Heap[2*s] == 0 && Heap[2*s+1] != 0){
+		if(Heap[2*s+1] > Heap[s]){
+			return ;
+		}else{
+			k = Heap[s*2+1];
+			Heap[2*s+1] = Heap[s];
+			Heap[s] = k;
+			HeapDownSort(2*s+1);
+		}
+	}else if(Heap[2*s] != 0 && Heap[2*s+1] == 0){
+		if(Heap[2*s] > Heap[s]){
+			return ;
+		}else{
+			k = Heap[s*2];
+			Heap[2*s] = Heap[s];
+			Heap[s] = k;
+			HeapDownSort(2*s);
+		}
+	}else{
+		if(Heap[s*2] > Heap[s*2+1]){
+			if(Heap[s*2+1] > Heap[s]){
+				return ;
+			}else{
+				k = Heap[s*2+1];
+				Heap[2*s+1] = Heap[s];
+				Heap[s] = k;
+				HeapDownSort(2*s+1);
+			}
+		}else{
+			if(Heap[2*s] > Heap[s]){
+				return ;
+			}else{
+				k = Heap[s*2];
+				Heap[2*s] = Heap[s];
+				Heap[s] = k;
+				HeapDownSort(2*s);
+			}
+		}
 	}
 }
 
-void HUp(){
-	int i = size-1;
-
-    while(1){ // 
-        int parent = i/2;
-        if (parent > -1 && Heap[parent] > Heap[i]){
-            int t = Heap[parent];
-            Heap[parent] = Heap[i];
-            Heap[i]=t;
-            i = parent;
-        } else {
-            break;
-        }
-    }
-}
 
 
-void Increase(int array[]){
-	for(int i = 0; i < 10; i++){
-		Heap[size++] = array[i];
-		HUp();
+void Delete(){
+	if(size == 0){
+		return ;
+	}else{
+		printf("\n%d\n",Heap[1]);
+  		Heap[1] = Heap[size];
+  		Heap[size] = 0;
+    	size -= 1;
+		HeapDownSort(1);
 	}
 }
 
-int Delete(){
+void Increase(int a){
+	size++;
+	
+	if(size == 1){
+		Heap[size] = a;
+	}else{
+		Heap[size] = a;
+		HUp(size);
+	}
 	
 	
-	int t = Heap[0];
-    Heap[0] = Heap[size-1];
-    size = size - 1;
-    
-    
-	int i = 0;
-
-    while(i<size){
-        int c1 = 2*(i+1);
-        int c2 = 2*(i+1) + 1;
-        int t;
-        if (c1 <= size) {
-            t = c1;
-        } else {
-            break;
-        }
-        if (c2 <= size && Heap[c1] > Heap[c2]){
-            t = c2;
-        }
-
-        if(Heap[i] >= Heap[t]) break;
-
-        int temp = Heap[i];
-        Heap[i] = Heap[t];
-        Heap[t] = temp;
-        i = t;
-    }
-    
-    return t;
+	
 }
 
+void Initial(int array[],int k,int s){
+	for(int i = k; i < s; i++){
+		array[i] = rand()%55536 + 10000;
+		Increase(array[i]);
+	}
+	
+	printf("Add %d new data\n\n",size);
+	
+	for(int i = s-1; i > 0; i--){
+		Delete();
+		
+	}
+	
+}
 
 
 int main(){
 	srand(time(NULL));
+	int b = rand()%600 + 600;
 	
-	Initial(Array,10);
+	Initial(Array,1,601);
+	printf("\n\n\n");
 	
-	Increase(Array);
-	
-	
-	for(int i = 0; i < 10; i++){
-		printf("\n%d \n",Heap[i]);
-	}
-	printf("\n\n");
-	
-	for(int i = 0; i < 10; i++){
-		printf("\n%d \n",Delete());
+	for(int i = 1; i < 601; i++){
+		Heap[i] = Array[i];
 	}
 	
-	
+	Initial(Array,601,601+b+1);
+
 	return 0;
 } 
